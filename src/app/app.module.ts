9c2a9b469b4520184d3e1fbf146e317e10f8bff4
Anelvin <amejiaroque@gmail.com>
2023-03-07 00:03:03 +0100

    Add internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -11,10 +14,22 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
